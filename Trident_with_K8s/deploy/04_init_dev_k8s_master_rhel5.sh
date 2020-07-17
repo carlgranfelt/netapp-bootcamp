e16244b7ce1750938127592b191fe8af860e0df9
Carl Granfelt <carl@granfelt.com>
2020-07-17 17:56:39 +0100

    Updated shell script 04

--- Trident_with_K8s/deploy/04_init_dev_k8s_master_rhel5.sh
+++ Trident_with_K8s/deploy/04_init_dev_k8s_master_rhel5.sh
@@ -4,6 +4,7 @@ echo "kubeadm init in the k8s master node"
 echo "#######################################################################################################"
 
 systemctl enable kubelet && systemctl start kubelet
+kubeadm reset -f
 kubeadm init --token abcdef.0123456789abcdef --v=5
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
